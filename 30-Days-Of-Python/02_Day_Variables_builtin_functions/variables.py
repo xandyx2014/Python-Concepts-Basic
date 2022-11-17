8120a98f6446f7483281378706cc1c49850a1de4
Andy Jesus <xandyx2014@gmail.com>
2022-11-16 20:44:59 -0400

    Basic concept python

---
+++ 30-Days-Of-Python/02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,47 @@
+"""
+Este es un Comentario 
+"""
+# Variables in Python
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+country = 'Finland'
+city = 'Helsinki'
+
+# Number
+age = 250
+# Bool
+is_married = True
+# List
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+# Dictionary
+person_info = {
+    'firstname':'Asabeneh', 
+    'lastname':'Yetayeh', 
+    'country':'Finland',
+    'city':'Helsinki'
+    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
